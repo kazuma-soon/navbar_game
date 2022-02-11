8f20e34aa499af142d6115c922389f2163a7d4c9
kazuma-soon <podradio2020@gmail.com>
2022-02-12 07:41:46 +0900

    Add: plays_3の追加#4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require("channels")
 
 require("src/plays_1");
 require("src/plays_2");
+require("src/plays_3");
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- app/javascript/src/plays_3.js
+++ app/javascript/src/plays_3.js
@@ -1,47 +1,48 @@
 $(function(){
   
-  $.each((new Array(5)), function() { //繰り返しを指定する
+  $.each((new Array(3)), function() { //繰り返しを指定する
 
     function move(){ //アニメーションをチェインで指定
-      $('.js-btn-2')
+      $('.js-btn-3')
       
       .animate({
-        left: '45%',
+        top: '300px',
       }, 1000, 'swing')
 
       .animate({
-        left: '15%'
+        top: '0', left: '90%', 
+      }, 1000, 'swing')
+      
+      .animate({
+        top: '300px', left: '90%',
+      }, 1000, 'swing')
+      
+      .animate({
+        top: '100px', left: '20%',
+      }, 1000, 'swing')
+      
+      .delay(800)
+
+      .animate({
+        top: '250px', left: '60%',
       }, 1000, 'swing')
 
       .animate({
-        left: '75%',
+        top: '10px', left: '10%',
       }, 1000, 'swing')
-      
-      .animate({
-        left: '45%',
-      }, 1000, 'swing')
-      
-      .animate({
-        left: '90%',
-      }, 1000, 'swing')
-      
-      .animate({
-        left: '0%',
-      }, 1000, 'swing')
-
     }
  
     move(); //定義した関数の実行
 
   });
 
-  $('.js-btn-2').animate({ //アラートを出してゲームオーバー
-    left: '90%'
+  $('.js-btn-3').animate({ //アラートを出してゲームオーバー
+    top: '0', left: '90%'
   },{
     duration: 2500,
     easing: 'swing',
     complete: function(){ //ページが変わってもアラートでてしまう。classをaddするか？
-      $('.js-message-2').show();
+      $('.js-message-3').show();
     }
   })
 
